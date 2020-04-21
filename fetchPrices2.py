a407ae3704ed792296d8d602d08be14ec762a4fa
RabidSheep55 <tharreaupierre@gmail.com>
2020-04-22 00:03:05 +0200

    Option to fetch prices from sky.lea.moe API

---
+++ fetchPrices2.py
@@ -0,0 +1,25 @@
+import requests as rq
+import json
+from datetime import datetime
+
+# URL to fetch bazaar data from (sky.lea.moe)
+BASE_URL = r"https://sky.lea.moe/api/bazaar"
+
+# Get prices from API
+response = rq.get(BASE_URL)
+if response.status_code == 200:
+    pricesList = response.json()
+else:
+    print(f"Error getting Bazaar Prices | Code: {response.status_code}")
+
+# Format list into dict with ID keys
+pricesDict = {}
+for item in pricesList:
+    pricesDict[item["id"]] = item["price"]
+
+# Set date for recording purposes
+pricesDict["time"] = datetime.now().strftime(r"%A %d %b - %H:%M")
+
+# Save item prices in json file
+with open(r"Resources/bazaarPrices.json", 'w') as file:
+    json.dump(pricesDict, file, indent=3)
